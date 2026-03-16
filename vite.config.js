5726332838d0e30d122968ad068aac6906a39712
Yash <yashraj24october@gmail.com>
2026-03-16 08:48:17 +0530

    Initial  commit

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000',
+        target: "https://noted-backend-6dwfk36q2-yash-rajs-projects-c4dfd886.vercel.app",
         changeOrigin: true,
       }
     }
